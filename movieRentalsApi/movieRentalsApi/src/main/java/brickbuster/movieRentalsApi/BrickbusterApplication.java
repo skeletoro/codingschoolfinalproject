145ec342815f2ea86bc808494ea5fb6ae79ed5a2
skeletoro <69654563+skeletoro@users.noreply.github.com>
2020-12-02 14:41:59 -0700

    first upload spring initializer

---
+++ movieRentalsApi/movieRentalsApi/src/main/java/brickbuster/movieRentalsApi/BrickbusterApplication.java
@@ -0,0 +1,13 @@
+package brickbuster.movieRentalsApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrickbusterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrickbusterApplication.class, args);
+	}
+
+}
---
+++ movieRentalsApi/movieRentalsApi/src/test/java/brickbuster/movieRentalsApi/BrickbusterApplicationTests.java
@@ -0,0 +1,13 @@
+package brickbuster.movieRentalsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrickbusterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
