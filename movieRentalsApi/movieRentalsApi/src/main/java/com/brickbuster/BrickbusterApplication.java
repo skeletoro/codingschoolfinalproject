3d6c0f77ca55a2a8b7b9a78115df3564b889ee4f
skeletoro <69654563+skeletoro@users.noreply.github.com>
2020-12-06 17:01:16 -0700

    updating package name

--- movieRentalsApi/movieRentalsApi/src/main/java/com/brickbuster/BrickbusterApplication.java
+++ movieRentalsApi/movieRentalsApi/src/main/java/com/brickbuster/BrickbusterApplication.java
@@ -1,4 +1,4 @@
-package brickbuster.movieRentalsApi;
+package com.brickbuster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
